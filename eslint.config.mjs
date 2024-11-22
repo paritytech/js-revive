9b109a0ac4dd63e249aedcb200f4707baef39d30
Sebastian Miasojed <sebastian.miasojed@parity.io>
2024-11-22 21:18:34 +0100

    Fmt

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,7 +6,7 @@ import tseslint from "typescript-eslint";
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {ignores: ["**/resolc.js"]},
+  {ignores: ["**/resolc.js", "**/worker.js"]},
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
