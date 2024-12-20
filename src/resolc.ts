7b061c76fab9b4b308f266683097e29eff7216b8
pgherveou <pgherveou@gmail.com>
2024-12-20 16:38:02 +0100

    rm comments

--- src/resolc.ts
+++ src/resolc.ts
@@ -5,11 +5,7 @@ import type { SolcOutput } from '.'
 export function resolc(input: string): SolcOutput {
     const m = Resolc() as any
     m.soljson = soljson
-
-    // Set input data for stdin
     m.writeToStdin(input)
-
-    // Compile the Solidity source code
     m.callMain(['--standard-json'])
     return JSON.parse(m.readFromStdout()) as SolcOutput
 }
